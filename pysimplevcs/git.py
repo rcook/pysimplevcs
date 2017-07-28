d98ee87b8c0223e58ec5bc047f52a4778456b90e
Richard Cook <rcook@rcook.org>
2017-07-28 15:52:53 -0700

    Implement Git.with_args to allow global arguments to be specified

--- pysimplevcs/git.py
+++ pysimplevcs/git.py
@@ -12,24 +12,14 @@ import os
 
 from pyprelude.file_system import make_path
 from pyprelude.process import execute, proxy_command
+from pyprelude.util import unpack_args
 
-class Git(object):
-    def __init__(self, cwd=None):
-        if cwd is None:
-            cwd = os.getcwd()
-
-        cwd = os.path.abspath(cwd)
-
-        self._is_bare = execute("git", "rev-parse", "--is-bare-repository", cwd=cwd).strip() == "true"
-
-        if self._is_bare:
-            self._repo_dir = cwd
-        else:
-            self._repo_dir = execute("git", "rev-parse", "--show-toplevel", cwd=cwd).strip()
-
-        self._git_dir = \
-            self._repo_dir if self._is_bare \
-            else make_path(self._repo_dir, ".git")
+class _Git(object):
+    def __init__(self, is_bare, repo_dir, git_dir, args):
+        self._is_bare = is_bare
+        self._repo_dir = repo_dir
+        self._git_dir = git_dir
+        self._args = args
 
     @property
     def is_bare(self): return self._is_bare
@@ -40,13 +30,42 @@ class Git(object):
     @property
     def git_dir(self): return self._git_dir
 
+    def with_args(self, *args):
+        return _Git(
+            self._is_bare,
+            self._repo_dir,
+            self._git_dir,
+            self._args + unpack_args(*args))
+
     def __getattr__(self, name):
         def _missing_method(*args, **kwargs):
             return proxy_command(
                 name,
                 self._repo_dir,
-                lambda n, args: ["git", n] + args,
+                lambda n, args: ["git"] + self._args + [n] + args,
                 *args,
                 **kwargs)
 
         return _missing_method
+
+class Git(_Git):
+    def __init__(self, cwd=None, args=[]):
+        if cwd is None:
+            cwd = os.getcwd()
+
+        cwd = os.path.abspath(cwd)
+
+        is_bare = execute(
+            "git",
+            "rev-parse",
+            "--is-bare-repository",
+            cwd=cwd).strip() == "true"
+
+        if is_bare:
+            repo_dir = cwd
+            git_dir = repo_dir
+        else:
+            repo_dir = execute("git", "rev-parse", "--show-toplevel", cwd=cwd).strip()
+            git_dir = make_path(repo_dir, ".git")
+
+        super(Git, self).__init__(is_bare, repo_dir, git_dir, args)
